61dba59e157ec223248f7e25da8f73f16b342c59
alec-r-reynolds <alechasrito@gmail.com>
2020-08-31 15:31:19 -0600

    SIgn in page close to done

--- src/App.js
+++ src/App.js
@@ -8,19 +8,42 @@ import Container from "@material-ui/core/Container";
 import CustomAppBar from "./components/app-bar";
 import RecipeViewer from "./components/viewRecipe";
 import SplashPage from "./components/splash";
+import { createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#88ffff",
+      main: "#4dd0e1",
+      dark: "#009faf",
+      contrastText: "#000000",
+    },
+    secondary: {
+      light: "#ffff81",
+      main: "#ffd54f",
+      dark: "#c8a415",
+      contrastText: "#000",
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Route path="/search/*" component={CustomAppBar} />
-      <Switch>
-        <Route path="/" exact component={SplashPage} />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Route path="/search/*" component={CustomAppBar} />
+        <Switch>
+          <Route path="/" exact component={SplashPage} />
 
-        <Route path="/search/ingredient" component={RecipeFinder} />
+          <Route path="/search/ingredient" component={RecipeFinder} />
 
-        <Route path="/recipes/:id" component={RecipeViewer} />
-      </Switch>
-    </Router>
+          <Route path="/search/recipes/:id" component={RecipeViewer} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/splash.js
+++ src/components/splash.js
@@ -1,9 +1,46 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import { Typography, Box } from "@material-ui/core";
+import { Typography, Box, ThemeProvider, withTheme } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import purple from "@material-ui/core/colors/purple";
+import { useTheme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
-export default class SplashPage extends React.Component {
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const mainColor = "#4dd0e1";
+
+const styles = (theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: mainColor,
+  },
+  test: {},
+  box: {
+    height: "100%",
+  },
+  grid: {
+    height: "100%",
+    padding: "30px",
+  },
+  paper: {
+    paddingRight: "30px",
+    paddingLeft: "30px",
+  },
+});
+
+class SplashPage extends React.Component {
   constructor(props) {
     super(props);
 
@@ -15,21 +52,126 @@ export default class SplashPage extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+    const { theme } = this.props;
     return (
-      <Container maxWidth="xl">
-        <Box mt={3}>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="stretch"
-          >
-            <Grid item>
-              <Typography>d</Typography>
+      <Box>
+        <Container maxWidth="xl" className={classes.root}>
+          <Box p={5}>
+            <Box mb={3}>
+              <Grid xs={4} container direction="row" justify="space-between">
+                <Grid container item sm={5} className={classes.test}>
+                  <Box>
+                    <Typography
+                      variant="h4"
+                      style={{ color: "white", fontWeight: "bold" }}
+                    >
+                      Recipeasy
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Grid
+              xs={12}
+              container
+              direction="row"
+              justify="center"
+              alignItems="stretch"
+            >
+              <Grid
+                container
+                item
+                alignItems="center"
+                direction="column"
+                xs={12}
+                md={3}
+                className={classes.test}
+              >
+                <Paper className={classes.paper}>
+                  <Box pb={8} mt={8}>
+                    <Box pb={2}>
+                      <Grid item>
+                        <Avatar>
+                          <LockOutlinedIcon />
+                        </Avatar>
+                      </Grid>
+                    </Box>
+                    <Grid item>
+                      <Typography variant="h4">Sign In</Typography>
+                    </Grid>
+
+                    <form className={classes.form} noValidate>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        InputLabelProps={{
+                          className: classes.helperText,
+                        }}
+                        InputProps={{
+                          classes: {
+                            notchedOutline: classes.notchedOutline,
+                            input: classes.textField,
+                          },
+                        }}
+                      />
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                      />
+
+                      <Box pt={2}>
+                        <Button
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                          className={classes.submit}
+                        >
+                          Sign In
+                        </Button>
+                      </Box>
+                      <Grid container>
+                        <Grid item xs>
+                          <Link href="#" variant="body2">
+                            Forgot password?
+                          </Link>
+                        </Grid>
+                        <Grid item>
+                          <Link href="#" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                          </Link>
+                        </Grid>
+                      </Grid>
+                    </form>
+                  </Box>
+                </Paper>
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-      </Container>
+          </Box>
+        </Container>
+      </Box>
     );
   }
 }
+
+SplashPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(SplashPage);
